45112427c089fb31fb6f9c5c7f16f9ddcb2d2925
Marcela Malaver <marcela9409@gmail.com>
2022-07-21 09:52:25 -0500

    ✨ Add Sidebar

--- pages/components/layouts/Layout.tsx
+++ pages/components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import Head from "next/head";
 import { FC, PropsWithChildren } from "react";
-import { Navbar } from "../ui";
+import { Navbar, Sidebar } from "../ui";
 
 interface Props{ 
     title?: string;
@@ -13,6 +13,7 @@ const Layout:FC<PropsWithChildren<Props>> = ({title='OpenJira', children}) => {
             <title>{title}</title>
         </Head>
         <Navbar />
+        <Sidebar />
         <Box sx={{padding:'10px 20px'}}>
             {children}
         </Box>
