468e03e828fcde8e8683a30624b8b343a52e8f25
Marcela Malaver <marcela9409@gmail.com>
2022-07-21 15:13:16 -0500

    💄 Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import { Typography } from '@mui/material'
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { Layout } from './components/layouts'
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">Hi world</Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="Pendientes" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="En Progreso" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="Completadas" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
