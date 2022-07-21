5c8c6e7c9c5d1d0727484767b322fbc270e8ec85
Marcela Malaver <marcela9409@gmail.com>
2022-07-21 15:39:18 -0500

    ✨ Add context and interface

---
+++ context/entries/EntriesProvider.tsx
@@ -0,0 +1,40 @@
+import { FC, PropsWithChildren, useReducer } from 'react';
+import { EntriesContext, entriesReducer } from './';
+import { Entry } from '../../interfaces/entry';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface EntriesState{
+     entries: Entry[];
+}
+const Entries_INITIAL_STATE: EntriesState = {
+     entries: [
+        {
+            _id: uuidv4(),
+            description: 'Prueba inicio1',
+            status: 'pending',
+            createdAt: Date.now(),
+        },
+        {
+            _id: uuidv4(),
+            description: 'Prueba inicio2',
+            status: 'in-progress',
+            createdAt: Date.now()-1000000,
+        },
+        {
+            _id: uuidv4(),
+            description: 'Prueba inicio3',
+            status: 'finished',
+            createdAt: Date.now()-100000,
+        }
+    ],
+}
+export const EntriesProvider:FC<PropsWithChildren> = ({children}) => {
+const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
+return (
+  <EntriesContext.Provider value={{
+     ...state,
+ }}>
+     {children}
+</EntriesContext.Provider>
+)
+}
---
+++ context/entries/index.ts
@@ -0,0 +1,3 @@
+export { EntriesContext } from './EntriesContext';
+export * from './EntriesProvider';
+export { entriesReducer } from './entriesReducer';
