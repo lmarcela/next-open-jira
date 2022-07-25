43962fb8f0b8c4cdc51cc9b2435df4d4ba5cc388
Marcela Malaver <marcela9409@gmail.com>
2022-07-25 09:27:57 -0500

    ✨ Initilization of add entry

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,68 @@
+import { Box, Button, TextField } from '@mui/material'
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import AddIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { ChangeEvent, useState } from 'react';
+
+export const NewEntry = () => {
+    const [isAdding, setIsAdding] = useState(false);
+    const [inputValue, setInputValue] = useState('second');
+    const [touched, setTouched] = useState(false);
+    const onTextFieldChanged = (event: ChangeEvent <HTMLInputElement | HTMLTextAreaElement>) => {
+        setInputValue(event.target.value);
+    }
+    const onSave=()=>{
+        if(inputValue.length===0) return;
+        console.log({inputValue});
+    }
+  return (
+    <>
+        <Box sx={{marginBottom: 2, paddingX:2}}>
+            {
+                isAdding ? (
+                    <>
+                    <TextField
+                        fullWidth
+                        sx={{marginTop: 2, marginBottom: 1}}
+                        placeholder="Nueva entrada"
+                        autoFocus
+                        multiline
+                        label="Nueva entrada"
+                        helperText={inputValue.length<=0 && touched && 'Ingrese un valor'}
+                        error={inputValue.length<=0 && touched}
+                        value={inputValue}
+                        onChange={onTextFieldChanged}
+                        onBlur={()=>setTouched(true)}
+                    />
+                    <Box display='flex' justifyContent='space-between'>
+                        <Button
+                            variant='text'
+                            onClick={()=>setIsAdding(false)}
+                        >
+                            Cancelar
+                        </Button>
+                        <Button
+                            variant='outlined'
+                            color='secondary'
+                            endIcon={<SaveOutlinedIcon />}
+                            onClick={onSave}
+                        >
+                            Guardar
+                        </Button>
+                    </Box>
+                    </>
+                ):(
+                    <Button 
+                        startIcon={<AddIcon />}
+                        fullWidth
+                        variant='outlined'
+                        onClick={()=>setIsAdding(true)}
+                    >
+                        Agregar Tarea
+                    </Button>
+                )
+            }
+            
+        </Box>
+    </>
+  )
+}
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from './Navbar';
 export { Sidebar } from './Sidebar';
 export { EntryList } from './EntryList';
 export * from './EntryCard';
+export * from './NewEntry';
